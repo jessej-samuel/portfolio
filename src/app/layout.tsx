c657bd19b7ad14d576ebabfb5543c89cb7683ab0
Jessej Samuel <1234jessej@gmail.com>
2023-11-07 12:14:12 +0530

    setup: base style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import { GeistSans, GeistMono } from "geist/font";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "Jessej Samuel",
@@ -17,7 +15,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={GeistSans.className}>{children}</body>
+      <body
+        className={`${GeistSans.className} max-w-4xl bg-geist-0 text-geist min-h-screen mx-auto px-4 sm:px-6 md:px-8`}
+      >
+        <div className="grid grid-cols-3">
+          <div className="px-8 py-4 border border-geist-200">
+            <Image
+              src={"/coder_pfp.png"}
+              width={100}
+              height={100}
+              alt="Jessej Samuel"
+              className="rounded-full border border-success-dark"
+            />
+            <h1 className="font-semibold text-2xl">Jessej Samuel</h1>
+          </div>
+          <div>{children}</div>
+        </div>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div>
-      <h1 className="font-semibold text-4xl">Jessej Samuel</h1>
-    </div>
-  );
+  return <h1>Hi</h1>;
 }

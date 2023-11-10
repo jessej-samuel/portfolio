c2455d1ead364863052d5a86dcbf4616bbfd8273
Jessej Samuel <1234jessej@gmail.com>
2023-11-10 11:39:05 +0530

    🎉 feat: stats component works!

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -1,4 +1,5 @@
 import { fetcher } from "@/utils/fetcher";
+import { Loader } from "@geist-ui/icons";
 import Image from "next/image";
 import { Dispatch, SetStateAction } from "react";
 import useSWR from "swr";
@@ -9,8 +10,34 @@ interface StatsProps {
 const Stats = ({ className = "", setCursorState }: StatsProps) => {
   const { data, error, isLoading } = useSWR("/api/visit", fetcher);
 
-  if (isLoading) return <p>Loading...</p>;
-  if (error) return <p>Error</p>;
+  if (isLoading)
+    return (
+      <div
+        onMouseEnter={() => setCursorState("stats")}
+        onMouseLeave={() => setCursorState("default")}
+        onMouseUp={(e) => {
+          setCursorState("stats");
+          e.stopPropagation();
+        }}
+        className={`${className}  text-black bg-geist rounded-xl border border-dimgray flex items-center justify-center`}
+      >
+        <Loader className="animate-spin" />
+      </div>
+    );
+  if (error)
+    return (
+      <div
+        onMouseEnter={() => setCursorState("stats")}
+        onMouseLeave={() => setCursorState("default")}
+        onMouseUp={(e) => {
+          setCursorState("stats");
+          e.stopPropagation();
+        }}
+        className={`${className} p-5 font-medium text-black bg-gradient-to-tr from-white/60 via-geist-700/90 to-white/60 rounded-xl border border-dimgray`}
+      >
+        <p>Error</p>
+      </div>
+    );
 
   return (
     <div
@@ -24,11 +51,11 @@ const Stats = ({ className = "", setCursorState }: StatsProps) => {
     >
       <div className="flex items-center gap-4 mb-2">
         <Image src={"/eye.svg"} width={24} height={24} alt="views icon" />
-        <p className="text-sm font-normal">1563 views</p>
+        <p className="text-sm font-normal">{data.views} views</p>
       </div>
       <div className="flex items-center gap-4 ">
         <Image src={"/users.svg"} width={24} height={24} alt="views icon" />
-        <p className="text-sm font-normal"> 463 visitors</p>
+        <p className="text-sm font-normal">{data.visitors} visitors</p>
       </div>
     </div>
   );

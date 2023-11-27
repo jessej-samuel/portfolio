0954e61bae8af6b8b7fa6aab16a7dfd1c53d64b2
Jessej Samuel <1234jessej@gmail.com>
2023-11-27 15:29:56 +0530

    Remove Spotify API route

--- src/app/api/spotify/route.ts
+++
@@ -1,7 +0,0 @@
-export async function GET() {
-  const res = {
-    clientId: process.env.SPOTIFY_CLIENT_ID,
-    clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-  };
-  return Response.json(res);
-}
